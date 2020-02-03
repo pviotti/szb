0a246aa918cc3ccc618497b27828252f1539978c
Paolo <pviotti@users.noreply.github.com>
2020-02-03 22:07:28 +0000

    pretty-print entry size

--- Program.fs
+++ Program.fs
@@ -19,13 +19,24 @@ let rec getSize path =
         else
             (new FileInfo(path)).Length
     with
-    | :? Exception as ex ->
+    | ex ->
         printfn "Error: %s" ex.Message
         0L
 
+let getSizeString (bytes:int64) =
+    let sizeUnit = [ "B"; "KiB"; "MiB"; "TiB"; "PiB"; "EiB" ]
+    if bytes = 0L then
+        (0.0, sizeUnit.[0])
+    else
+        let bytesF = float(bytes)
+        let sizeUnitIdx = Math.Floor(Math.Log(bytesF, 1024.0))
+        let num = Math.Round(bytesF / Math.Pow(1024.0, sizeUnitIdx), 0)
+        (num, sizeUnit.[int(sizeUnitIdx)])
+
 let printFormatted (path:string, size:int64) =
     let name = Array.last (path.Split '/')
-    printfn "%10iB  %s" size name
+    let (size, sizeUnit) = getSizeString size
+    printfn "%10.0f %-3s %s" size sizeUnit name
 
 
 [<EntryPoint>]
@@ -41,5 +52,6 @@ let main argv =
             Seq.iter printFormatted
     print lsD sizeD
     print lsF sizeF
-    printfn "-----------\n%10iB" sizeTot
+    printfn "%s" (String.replicate 14 "-")
+    printFormatted("", sizeTot)
     0
