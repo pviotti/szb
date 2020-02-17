77053b380b8c8b14bb1d44fe056dfa6631be0d68
Paolo <pviotti@users.noreply.github.com>
2020-02-17 18:47:56 +0000

    add getSizeString tests

--- Sizy.Tests/Tests.fs
+++ Sizy.Tests/Tests.fs
@@ -4,13 +4,28 @@ open System
 open Xunit
 open FsUnit.Xunit
 
+open Sizy
+
 module ``Sizy tests`` =
 
-    type SizyTests () =
+    let getSizeStringTestData : Object [] [] =
+        [|
+            [| 0; 0.0; "B" |]
+            [| 1024; 1.0; "k" |]
+            [| 1024+103; 1.0; "k" |]
+            [| 1024*1024; 1.0; "M" |]
+            [| Math.Pow(1024.0,3.0); 1.0; "G" |]
+        |]
 
-        [<Fact>]
-        member __.``It should exit with 0`` () =
-            Sizy.Program.main([||]) |> should equal 0
+    [<Fact>]
+    let ``It should exit with 0`` () =
+        Program.main([||]) |> should equal 0
+
+    [<Theory>]
+    [<MemberData("getSizeStringTestData")>]
+    let ``getSizeString`` (input:int64, outSize:float, outUnit:string) =
+        Program.getSizeString input |> should equal (outSize, outUnit)
+
 
 
 [<EntryPoint>]
