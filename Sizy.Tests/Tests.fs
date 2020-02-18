95fe48260e9b07ed333b6e0fc297a961135973e5
Paolo Viotti <pviotti@microsoft.com>
2020-02-18 10:49:20 +0000

    more functional test data array generation

--- Sizy.Tests/Tests.fs
+++ Sizy.Tests/Tests.fs
@@ -15,8 +15,7 @@ let chars =
            [| 'A' .. 'Z' |]
            [| '0' .. '9' |] ])
 
-let charsLen = Array.length chars
-let rndStr n = String(Array.init n (fun _ -> chars.[r.Next charsLen]))
+let rndStr n = String(Array.init n (fun _ -> chars.[r.Next (Array.length chars)]))
 let sep = string Path.DirectorySeparatorChar
 
 module ``Sizy tests`` =
@@ -24,20 +23,19 @@ module ``Sizy tests`` =
     let createTestFileSystem n =
         let tmpPath = Path.GetTempPath()
         let rootFolder = Directory.CreateDirectory(tmpPath + sep + (rndStr 5))
-        use fs1 = new FileStream(rootFolder.FullName + sep + (rndStr 5), FileMode.CreateNew)
-        use bw1 = new BinaryWriter(fs1)
-        for x in [ 1 .. n ] do
-            bw1.Write 1uy
+        use fs = new FileStream(rootFolder.FullName + sep + (rndStr 5), FileMode.CreateNew)
+        use bw = new BinaryWriter(fs)
+        Array.init n (fun _ -> 1uy) |> Array.iter bw.Write
         rootFolder.FullName
 
     let getSizeStringTestData: Object [] [] =
         [| [| -12345; 0.0; "B" |]
            [| 0; 0.0; "B" |]
            [| 1024; 1.0; "k" |]
-           [| 1024 + 103
+           [| (1024 + 103)
               1.0
               "k" |]
-           [| 1024 * 1024
+           [| (1024 * 1024)
               1.0
               "M" |]
            [| Math.Pow(1024.0, 3.0)
@@ -49,10 +47,6 @@ module ``Sizy tests`` =
         let rootFolder = createTestFileSystem 100
         Program.sizyMain rootFolder |> should equal 100.0
 
-    //[<Fact>]
-    //let ``It should exit with 0`` () =
-    //    Program.main([||]) |> should equal 0
-
     [<Theory>]
     [<MemberData("getSizeStringTestData")>]
     let getSizeString (input: int64, outSize: float, outUnit: string) =
@@ -61,4 +55,4 @@ module ``Sizy tests`` =
 
 
 [<EntryPoint>]
-let main argv = 0
+let main _ = 0
