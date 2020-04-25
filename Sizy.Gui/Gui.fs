9d36dc8ca264bd390dc1578cd375bff80cac1d46
Paolo <pviotti@users.noreply.github.com>
2020-04-25 23:05:29 +0100

    gui: add backward browsing

--- Sizy.Gui/Gui.fs
+++ Sizy.Gui/Gui.fs
@@ -6,14 +6,19 @@ open NStack
 open System.IO
 open Sizy.Main
 open Sizy.Config
+open System.IO.Abstractions
 open System.Collections.Concurrent
+open FSharp.Collections.ParallelSeq
 
 let ustr (x: string) = ustring.Make(x)
 
 #region "Data and related functions"
-let mutable pwd = ""
-let mutable lstData : string [] = [||]
+
+let mutable dirStack: string list = []
+let mutable lstData: string [] = [||]
 let mutable totSizeStr = ""
+let fsEntries = ConcurrentDictionary<string, Entry>()
+let errors = ConcurrentDictionary<string, string>()
 
 let getEntries ls (fsEntries: ConcurrentDictionary<string, Entry>) =
     let getStrSeq f =
@@ -29,54 +34,71 @@ let getTotalSizeStr totSize =
     let totSize, totSizeUnit = getSizeUnit totSize
     sprintf "Tot. %10.0f%s" totSize totSizeUnit
 
-let updateData path =
-    let ls, entries, totSize, _ = sizyMain path
+let updateData path entries =
+    let ls = Directory.EnumerateFileSystemEntries path
+    let sizes = PSeq.map (getSize (FileSystem()) entries errors) ls
+    totSizeStr <- getTotalSizeStr (PSeq.sum sizes)
     lstData <- getEntries ls entries
-    totSizeStr <- getTotalSizeStr totSize
-#endregion 
 
-#region "UI controls"
-let win =
+#endregion
+
+#region "UI components"
+
+let window =
     { new Window(ustr "Sizy", X = Pos.op_Implicit (0), Y = Pos.op_Implicit (0), Width = Dim.Fill(), Height = Dim.Fill()) with
-        member u.ProcessKey(k: KeyEvent) =
+        member __.ProcessKey(k: KeyEvent) =
             if k.KeyValue = int 'q' then
                 Application.Top.Running <- false
                 true
             else
                 base.ProcessKey k }
 
-let lblTotSize = Label (ustr "", X = Pos.At(0), Y = Pos.AnchorEnd(1), Width = Dim.Fill(), Height = Dim.Sized(1))
-let lstView = { new ListView([||], X = Pos.At(0), Y = Pos.At(0), Width = Dim.Percent(50.0f), Height = Dim.Fill(1)) with
-            member u.ProcessKey(k: KeyEvent) =
-                let entryName = lstData.[u.SelectedItem].Substring(13)
-                if k.Key = Key.Enter && entryName.EndsWith "/" then
-                    pwd <- pwd + "/" + entryName.TrimEnd('/')
-                    updateData pwd
-                    Application.MainLoop.Invoke(fun () ->
-                            u.SetSource lstData
-                            lblTotSize.Text <- ustr totSizeStr
-                        )
-                    true
-                else
-                    base.ProcessKey k }
+let lblTotSize = Label(ustr "", X = Pos.At(0), Y = Pos.AnchorEnd(1), Width = Dim.Fill(), Height = Dim.Sized(1))
+
+let lstView =
+    { new ListView([||], X = Pos.At(0), Y = Pos.At(0), Width = Dim.Percent(50.0f), Height = Dim.Fill(1)) with
+        member u.ProcessKey(k: KeyEvent) =
+            let entryName = lstData.[u.SelectedItem].Substring(13)
+            if (k.Key = Key.Enter || k.Key = Key.CursorRight) && entryName.EndsWith "/" then
+                let newDir = List.head (dirStack) + "/" + entryName.TrimEnd('/')
+                dirStack <- newDir :: dirStack
+                updateData newDir fsEntries
+                Application.MainLoop.Invoke(fun () ->
+                    u.SetSource lstData
+                    lblTotSize.Text <- ustr totSizeStr)
+                true
+            elif (k.KeyValue = int 'b' || k.Key = Key.CursorLeft) && List.length dirStack > 1 then
+                dirStack <-
+                    match dirStack with
+                    | _ :: tl -> tl
+                    | [] -> []
+                updateData (List.head dirStack) fsEntries
+                Application.MainLoop.Invoke(fun () ->
+                    u.SetSource lstData
+                    lblTotSize.Text <- ustr totSizeStr)
+                true
+            else
+                base.ProcessKey k }
+
 #endregion
 
 [<EntryPoint>]
 let main argv =
     match getConfiguration argv with
     | Config config ->
-        pwd <- if config.Contains InputPath then config.GetResult InputPath else Directory.GetCurrentDirectory()
+        let path =
+            if config.Contains InputPath then config.GetResult InputPath else Directory.GetCurrentDirectory()
         Application.Init()
 
-        updateData pwd
+        dirStack <- [ path ]
+        updateData path fsEntries
         Application.MainLoop.Invoke(fun () ->
-                lstView.SetSource lstData
-                lblTotSize.Text <- ustr totSizeStr
-            )
+            lstView.SetSource lstData
+            lblTotSize.Text <- ustr totSizeStr)
 
-        win.Add(lstView)
-        win.Add(lblTotSize)
-        Application.Top.Add(win)
+        window.Add(lstView)
+        window.Add(lblTotSize)
+        Application.Top.Add(window)
         Application.Run()
         0
     | ReturnVal ret -> ret
