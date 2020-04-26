c9221eb015840a84a8bbdd4e3bc0b1d8feec2409
Paolo <pviotti@users.noreply.github.com>
2020-04-26 09:39:27 +0100

    simplify config

--- Sizy/Config.fs
+++ Sizy/Config.fs
@@ -9,13 +9,11 @@ let PROGRAM_NAME = "sizy"
 
 type Args =
     | [<NoAppSettings>] Version
-    | [<AltCommandLine("-p")>] Percentage
     | [<MainCommand>] InputPath of path: string
     interface IArgParserTemplate with
         member s.Usage =
             match s with
             | Version _ -> sprintf "print %s version." PROGRAM_NAME
-            | Percentage _ -> "show percentage values"
             | InputPath _ -> "the folder you want to analyse"
 
 type ConfigOrInt =
@@ -26,14 +24,7 @@ let printVersion() = printfn "%s version %s" PROGRAM_NAME VERSION
 
 
 let getConfiguration argv =
-    let errorHandler =
-        ProcessExiter
-            (colorizer =
-                function
-                | ErrorCode.HelpText -> None
-                | _ -> Some ConsoleColor.Red)
-
-    let parser = ArgumentParser.Create<Args>(programName = PROGRAM_NAME, errorHandler = errorHandler)
+    let parser = ArgumentParser.Create<Args>(programName = PROGRAM_NAME, errorHandler = ProcessExiter())
     let config = parser.Parse(argv, ignoreMissing = true)
 
     if config.Contains Version then
